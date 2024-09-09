c5629fb70c836e0b5cf0961f68838ec9785a2488
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-09-09 18:22:43 +0530

    pretty basic ngl

--- nodemon-go/main.go
+++ nodemon-go/main.go
@@ -1,7 +1,37 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"sync"
+	"time"
+)
 
-func main () {
-	
+var (
+	cmd           *exec.Cmd
+	watchDirs     []string
+	watchPatterns []string
+	restartDelay  = time.Second
+	mu            sync.Mutex
+	preRestart    func()
+	postRestart   func()
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage : gomon <command> [args...]")
+		os.Exit(1)
+	}
+
+	watchDirs = []string{"."}
+	if envDirs := os.Getenv("GOMON_WATCH"); envDirs != "" {
+		watchDirs = strings.Split(envDirs, ",")
+	}
+
+	watchPatterns = []string{"*.go"}
+	if envPatterns := os.Getenv("GOMON_WATCH"); envPatterns != "" {
+		watchPatterns = strings.Split(envPatterns, ",")
+	}
 }
