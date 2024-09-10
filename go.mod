fe4eb293c4e8f8d7500a77851970a6c06b425051
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-09-10 12:38:04 +0530

    pretty basic ngl

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/mrinalxdev/getting-cracked-in-go
 go 1.21.5
 
 require (
-	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0
 	golang.org/x/sys v0.4.0 // indirect
 )
--- nodemon-go/main.go
+++ nodemon-go/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 var (
@@ -34,4 +38,89 @@ func main() {
 	if envPatterns := os.Getenv("GOMON_WATCH"); envPatterns != "" {
 		watchPatterns = strings.Split(envPatterns, ",")
 	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	for _, dir := range watchDirs {
+		err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				return watcher.Add(path)
+			}
+			return nil
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	go watchForChanges(watcher)
+}
+
+func watchForChanges(watcher *fsnotify.Watcher) {
+	debounce := time.NewTimer(restartDelay)
+	debounce.Stop()
+
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				if matchesPattern(event.Name) {
+					if isHotReloadable(event.Name) {
+						hotReload(event.Name)
+					} else {
+						debounce.Reset(restartDelay)
+					}
+				}
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			log.Println("error : ", err)
+		case <-debounce.C:
+			restartProcess()
+		}
+	}
+}
+
+func restartProcess() {
+	stopProcess()
+	startProcess()
+}
+
+func startProcess() {
+
+}
+
+func stopProcess() {
+
+}
+
+func isHotReloadable(filename string) bool {
+	return filepath.Ext(filename) != ".go"
+}
+
+func hotReload(filename string) {
+	log.Printf("Hot reloading change in file : %s\n", filename)
+	touchFile := "/tmp/hot_reload_trigger"
+	exec.Command("touch", touchFile).Run()
+}
+
+func matchesPattern(filename string) bool {
+	for _, pattern := range watchPatterns {
+		if matched, _ := filepath.Match(pattern, filepath.Base(filename)); matched {
+			return true
+		}
+	}
+	return true
 }
